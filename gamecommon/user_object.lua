f2b07c3ed41d0fb203398e715e621d0255913810
xiaou <xiaotjyu@gmail.com>
2016-04-01 14:00:25 +0800

    倾斜一点点，耶

--- gamecommon/user_object.lua
+++ gamecommon/user_object.lua
@@ -87,6 +87,9 @@ function __user_object:enterScene( scene_name, x, y )
         self.scene_node = TLSeamlessMap:create( scene_name, x, y )
         all_scene_layers[layer_type_scene]:addChild( self.scene_node )
 
+        local camera = self.scene_node:getCamera();
+        camera:setEyeXYZ( 0, -10, 10 );
+
         if not self.model_obj then
             self.model_obj = ( require 'gamecommon.model_object' ).new( self.scene_node, self.save_datas.model_id )
             self.model_obj:playAction( 'standby', -1 )
