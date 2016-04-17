d1d09f34673c76912a4da71b4a10ab0b0695171c
xiaou <yyolq0410@163.com>
2016-04-18 01:01:48 +0800

    fix bug

--- Editor/main.lua
+++ Editor/main.lua
@@ -50,7 +50,10 @@ function main()
         local grid_height = tonumber( new_args[6] )
         local material = new_args[7]
 
+        CCLuaLog( 'args : ' .. tostring( args ) )
+        CCLuaLog( 'new_args .. ' .. #new_args )
         local new_sm_node = TLSeamlessMap:newSeamlessMap( file_name, block_name, block_row, block_col, grid_width, grid_height, material );
+        CCLuaLog( 'new_sm_node : ' .. tostring( new_sm_node ) )
         if new_sm_node then
             if g_sm_node then g_sm_node:removeFromParentAndCleanup( true ) end
 
@@ -99,6 +102,7 @@ function main()
     local last_pos_y = nil
     local sm_move_flag = false              -- 平移大地图
     local rotation_flag = false             -- 旋转
+    local copy_flag = false
     g_edit_node = nil
     local last_edit_node_x = nil
     local last_edit_node_y = nil
@@ -111,10 +115,11 @@ function main()
         last_pos_y = down_args[2]
         local ctrl_flag = ( down_args[3] == 'true' )
         local shift_flag = ( down_args[4] == 'true' )
+        local alt_flag = ( down_args[5] == 'true' )
 
         down_flag = true
 
-        if ctrl_flag then                       -- 旋转
+        if shift_flag then                       -- 旋转
             if g_edit_mb and g_edit_node then
                 down_x, down_y = convertPointToEditMB( last_pos_x, last_pos_y )
                 if down_x then
@@ -124,8 +129,13 @@ function main()
                     last_edit_node_rotation = g_edit_node:getRotation()
                 end
             end
-        elseif shift_flag then                  -- 平移大地图
+        elseif alt_flag then                  -- 平移大地图
             sm_move_flag = true
+        elseif ctrl_flag then
+            if g_edit_mb and g_edit_node then
+                copy_flag = true
+                g_edit_node = g_edit_mb:copyObject( g_edit_node )
+            end
         else                                    -- 选择
             local w_x, w_y = convertPointToSM( last_pos_x, last_pos_y )
             if w_x then
@@ -144,9 +154,11 @@ function main()
         last_pos_y = down_args[2]
         local ctrl_flag = ( down_args[3] == 'true' )
         local shift_flag = ( down_args[4] == 'true' )
+        local alt_flag = ( down_args[5] == 'true' )
 
         down_flag = false
         sm_move_flag = false
+        copy_flag = false
 
         if rotation_flag then
             if g_edit_mb and g_edit_node then
@@ -166,17 +178,20 @@ function main()
         local pos_y = down_args[2]
         local ctrl_flag = ( down_args[3] == 'true' )
         local shift_flag = ( down_args[4] == 'true' )
+        local alt_flag = ( down_args[5] == 'true' )
 
         -- 
         local offset_x = ( pos_x - last_pos_x ) / g_main_scale
         local offset_y = ( pos_y - last_pos_y ) / g_main_scale
+        CCLuaLog( 'alt_flag : ' .. tostring( alt_flag ) )
+        CCLuaLog( 'offset_y : ' .. tostring( offset_y ) )
 
         last_pos_x = pos_x
         last_pos_y = pos_y
 
         -- 
-        if rotation_flag then
-            if ctrl_flag then
+        if shift_flag then
+            if rotation_flag then
                 local m_x, m_y = convertPointToEditMB( last_pos_x, last_pos_y )
                 if m_x then
                     local v1 = { x = down_x - last_edit_node_x, y = down_y - last_edit_node_y, z = 0 }
@@ -186,7 +201,7 @@ function main()
                     local v_cross = vector_cross( v1, v2 )
                     local dis = vector_distance( v1, v2 )
 
-                    local angle = math.acos( dot_value / dis ) * 180.0 / M_PI;
+                    local angle = math.acos( dot_value / dis ) * 180.0 / math.pi;
 
                     if v_cross.z > 0 then
                         g_edit_node:setRotation( last_edit_node_rotation - angle )
@@ -195,15 +210,19 @@ function main()
                     end
                 end
             end
-        elseif sm_move_flag then
-            if shift_flag and g_sm_node then
+        elseif alt_flag then
+            if sm_move_flag and g_sm_node then
                 local x = g_sm_node:getPositionX() + offset_x
                 local y = g_sm_node:getPositionY() - offset_y
                 g_sm_node:setPosition( x, y )
             end
+        elseif ctrl_flag then
+            if copy_flag and g_edit_mb and g_edit_node then
+                g_edit_mb:moveObject( g_edit_node, offset_x, -offset_y )
+            end
         else
             if down_flag and g_edit_mb and g_edit_node then
-                g_edit_mb:moveObject( g_edit_node, offet_x, -offset_y )
+                g_edit_mb:moveObject( g_edit_node, offset_x, -offset_y )
             end
         end
     end)
